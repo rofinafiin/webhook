f0f98094690cc2778db569ab30e6461c2f19667a
rofinafiin <rofinafiisr@gmail.com>
2023-11-10 14:10:14 +0000

    update dependencies

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ toolchain go1.21.4
 require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.8.0
 	github.com/aiteung/atapi v0.1.1
-	github.com/aiteung/atdb v0.1.3
 	github.com/aiteung/atmessage v0.1.0
 	github.com/aiteung/module v0.1.0
 	github.com/whatsauth/wa v0.0.4
@@ -19,6 +18,7 @@ require (
 	aidanwoods.dev/go-result v0.1.0 // indirect
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/Rhymen/go-whatsapp v0.1.1 // indirect
+	github.com/aiteung/atdb v0.1.3 // indirect
 	github.com/aiteung/musik v0.0.5 // indirect
 	github.com/andybalholm/brotli v1.0.6 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
--- handler.go
+++ handler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/whatsauth/wa"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func PostBalasan(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,61 @@ func PostBalasan(w http.ResponseWriter, r *http.Request) {
 			To:       msg.Phone_number,
 			IsGroup:  false,
 			Messages: "Hai Hai Haiii kamuuuui " + msg.Alias_name + "rofinya lagi gaadaa \n aku giseuubott salam kenall yaaaa \n",
+		}
+		resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
+
+func ReverseGeocode(latitude, longitude float64) (string, error) {
+	// OSM Nominatim API endpoint
+	apiURL := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f", latitude, longitude)
+
+	// Make a GET request to the API
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	// Extract the place name from the response
+	displayName, ok := result["display_name"].(string)
+	if !ok {
+		return "", fmt.Errorf("unable to extract display_name from the API response")
+	}
+
+	return displayName, nil
+}
+
+func Liveloc(w http.ResponseWriter, r *http.Request) {
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+
+	// Reverse geocode to get the place name
+	location, err := ReverseGeocode(msg.Latitude, msg.Longitude)
+	if err != nil {
+		// Handle the error (e.g., log it) and set a default location name
+		location = "Unknown Location"
+	}
+
+	reply := fmt.Sprintf("Hai hai haiii kamu pasti lagi di %s \n Koordinatenya : %s - %s\n", location,
+		strconv.Itoa(int(msg.Longitude)), strconv.Itoa(int(msg.Latitude)))
+
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		dt := &wa.TextMessage{
+			To:       msg.Phone_number,
+			IsGroup:  false,
+			Messages: reply,
 		}
 		resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
 	} else {
